714f41fdfe2995e79eddbb2836e5e9d8f8a93ac9
developiaa <developiaa@gmail.com>
2021-09-11 11:13:59 +0900

    fix : Modify error

--- src/codility/lesson4/PermCheck2.java
+++ src/codility/lesson4/PermCheck2.java
@@ -14,7 +14,7 @@ public class PermCheck2 {
     //순열인지(1) 아닌지(0)에 따라서 결과값 반환
     public static int solution(int[] A) {
         Arrays.sort(A);
-        for (int i = 1; i < A.length; i++) {
+        for (int i = 1; i <= A.length; i++) {
             if (A[i - 1] != i) {
                 // 1부터 시작하기 때문에 배열 인덱스 0번째와 1의 값이 같아야한다.
                 // 중간 값이 하나라도 없으면 순열이 아니므로 0반환
