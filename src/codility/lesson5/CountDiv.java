cc853d8c46caa029c5f05f794c92edd29e9018c5
developiaa <developiaa@gmail.com>
2021-09-17 23:00:38 +0900

    feat : Add CountDiv #1

--- src/codility/lesson5/CountDiv.java
+++ src/codility/lesson5/CountDiv.java
@@ -1,2 +1,22 @@
-package codility.lesson5;public class CountDiv {
+package codility.lesson5;
+
+//쉬운 예제 풀이
+//그렇기 때문에 performance 0%
+public class CountDiv {
+    public static void main(String[] args) {
+        int A=6;
+        int B=11;
+        int K=2;
+        System.out.println(solution(A, B, K));
+    }
+
+    public static int solution(int A, int B, int K) {
+        int count = 0;
+        for (int i = A; i <= B; i++) {
+            if (i % K == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
