7f9cdf728d24c4705a22baca52ffe4d791f437a1
developiaa <developiaa@gmail.com>
2021-10-12 21:29:54 +0900

    test : recursive sum

--- src/basic/recursive.java
+++ src/basic/recursive.java
@@ -1,2 +1,20 @@
-package basic;public class recursive {
+package basic;
+
+public class recursive {
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+        System.out.println(sum2(5));
+    }
+
+    public static int sum(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return num + sum(num - 1);
+    }
+
+    public static int sum2(int num) {
+        return (num * (num + 1)) / 2;
+    }
+
 }
