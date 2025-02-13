7afa17c25b533c64b9f6614d6452974752ef4b9e
장경재 <developiaa@gmail.com>
2025-02-13 20:00:00 +0900

    feat: Baekjoon - 2744 대소문자 바꾸기

---
+++ src/baekjoon/_2744/Main.java
@@ -0,0 +1,21 @@
+package baekjoon._2744;
+
+import java.util.Scanner;
+
+public class Main {
+    // wRONGaNSWER
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if ('A' <= ch && ch <= 'Z') {
+                chars[i] = (char) ('a' + ch - 'A');
+            } else {
+                chars[i] = (char) ('A' + ch - 'a');
+            }
+        }
+        System.out.println(chars);
+    }
+}
