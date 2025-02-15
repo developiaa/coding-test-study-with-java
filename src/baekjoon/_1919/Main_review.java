9381dcba59d9139be59801675a30dd22f36e132e
장경재 <developiaa@gmail.com>
2025-02-15 11:00:00 +0900

    review: Baekjoon

---
+++ src/baekjoon/_1919/Main_review.java
@@ -0,0 +1,32 @@
+package baekjoon._1919;
+
+import java.util.Scanner;
+
+public class Main_review {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String first = sc.next();
+        String second = sc.next();
+
+        int[] firstInt = getAlphabet(first);
+        int[] secondInt = getAlphabet(second);
+
+        int answer = 0;
+        for (int i = 0; i < 26; i++) {
+            answer += Math.abs(firstInt[i] - secondInt[i]);
+        }
+
+        System.out.println(answer);
+
+    }
+
+    public static int[] getAlphabet(String string) {
+        int[] answer = new int[26];
+        for (int i = 0; i < string.length(); i++) {
+            answer[string.charAt(i) - 'a']++;
+        }
+        return answer;
+
+    }
+
+}
---
+++ src/baekjoon/_2744/Main_review.java
@@ -0,0 +1,22 @@
+package baekjoon._2744;
+
+import java.util.Scanner;
+
+public class Main_review {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.next();
+        char[] chars = string.toCharArray();
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            if ('A' <= c && c <= 'Z') {
+                // 대문자 -> 소문자
+                chars[i] = (char) (c - 'A' + 'a');
+            } else if ('a' <= c && c <= 'z') {
+                // 소문자 -> 대문자
+                chars[i] = (char) (c - 'a' + 'A');
+            }
+        }
+        System.out.println(chars);
+    }
+}
