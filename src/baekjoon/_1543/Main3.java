6ad610e8e5c8fcd80076a841f264e5f67017abe2
장경재 <developiaa@gmail.com>
2025-02-18 20:00:00 +0900

    feat: Baekjoon - 1543 문서 검색

---
+++ src/baekjoon/_1543/Main3.java
@@ -0,0 +1,15 @@
+package baekjoon._1543;
+
+import java.util.Scanner;
+
+// 문서 검색
+public class Main3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String doc = sc.nextLine();
+        String word = sc.nextLine();
+
+        String replace = doc.replace(word, "");
+        System.out.println((doc.length() - replace.length()) / word.length());
+    }
+}
