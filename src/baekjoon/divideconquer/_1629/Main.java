87f0dff3c4f337f4a8f9b49bce36b3f4f34e5a68
장경재 <developiaa@gmail.com>
2025-06-29 10:00:00 +0900

    feat: Baekjoon - 1629 곱셈

---
+++ src/baekjoon/divideconquer/_1629/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.divideconquer._1629;
+
+import java.util.Scanner;
+
+public class Main {
+    static int a, b, c;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        System.out.println(power(a, b));
+    }
+
+    static long power(int a, int b) {
+        if (b == 1) return a % c;
+        long half = power(a, b / 2);
+        if (b % 2 == 0) {
+            return (half * half) % c;
+        } else {
+            return (((half * half) % c) * a) % c;
+        }
+    }
+}
