704ef509c8355057f9ac13d26b3c9f5350403fa0
장경재 <developiaa@gmail.com>
2025-08-21 20:00:00 +0900

    feat: Baekjoon - 15657 N과 M (8)

---
+++ src/baekjoon/recursion/_15657/Main.java
@@ -0,0 +1,45 @@
+package baekjoon.recursion._15657;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] arr;
+    static int[] answer;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        answer = new int[M];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        perm(0, 0);
+        System.out.println(sb);
+
+    }
+
+    public static void perm(int depth, int start) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(answer[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            answer[depth] = arr[i];
+            perm(depth + 1, i);
+        }
+
+    }
+}
