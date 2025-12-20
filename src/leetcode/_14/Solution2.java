0b4131a560af5eac1a3c5f78d53097ae2ab8bc78
장경재 <developiaa@gmail.com>
2025-12-20 10:00:00 +0900

    feat: Leetcode - 14 Longest Common Prefix

---
+++ src/leetcode/_14/Solution2.java
@@ -0,0 +1,22 @@
+package leetcode._14;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        String[] strs = {"fl", "flower"};
+        System.out.println(longestCommonPrefix2(strs));
+    }
+
+    public static String longestCommonPrefix2(String[] strs) {
+        for (int i = 0; i < strs.length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+
+        return strs[0];
+    }
+
+}
