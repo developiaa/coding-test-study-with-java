0d0a529ccdc3749025bf6f797c968c8da35097d3
장경재 <developiaa@gmail.com>
2026-01-01 10:00:00 +0900

    feat: Leetcode - 83 Remove Duplicates from Sorted List

---
+++ src/leetcode/_83/Solution.java
@@ -0,0 +1,37 @@
+package leetcode._83;
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode current = head;
+
+        while (current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
